4d3df2b7e1aae8e7ed3476e1310bda3e7a8d4f4b
Shalom David Effiom <shalom@gravitasfinancellc.com>
2021-08-25 00:53:11 +0100

    Server Configs for Heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(4000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
